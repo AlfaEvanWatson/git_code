8d8dca74ea5703a8edf9e37e05c5088f2a5d17d4
evan <evan_watson@163.com>
2019-10-27 11:18:03 +0800

    modified the main.py plus last name

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 
 def hello_emma():
-    print("Hello, Emma!")
+    print("Hello, Emma!" + lastname())
 
 def hello_evan():
-    print("Hello, Evan!")
+    print("Hello, Evan!" + lastname())
+
+def lastname():
+    return " Watson"
 
 if __name__ == "__main__":
     hello_emma()
