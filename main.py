2f72aae178fd4cd065be8b0828582f9f011d561f
AlfaEvanWatson <32250596+AlfaEvanWatson@users.noreply.github.com>
2019-10-27 10:41:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 
 
-def hello():
-    print("Hello, world!")
+def hello_emma():
+    print("Hello, Emma!")
+
+def hello_evan():
+    print("Hello, Evan!")
 
 if __name__ == "__main__":
-    hello()
+    hello_emma()
+    print()
+    hello_evan()
